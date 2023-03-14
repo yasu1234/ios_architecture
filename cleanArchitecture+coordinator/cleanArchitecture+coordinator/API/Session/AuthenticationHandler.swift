1945fc93df2c0430e260fdc3e52e752937273a2e
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2023-03-15 03:38:11 +0900

    add API Request

---
+++ cleanArchitecture+coordinator/cleanArchitecture+coordinator/API/Session/AuthenticationHandler.swift
@@ -0,0 +1,62 @@
+import Alamofire
+
+final class AuthenticationHandler {
+    enum APIErrorResponseCode: Int, CaseIterable {
+        case authorizationFailed = 401
+        case headerLacking = 403
+    }
+    private let retryCount = 4
+    
+    private var isRefreshing = false
+    
+    private let session = Session()
+}
+
+extension AuthenticationHandler: RequestInterceptor {
+    // if you want to add header, add in this function
+    func adapt(_ urlRequest: URLRequest,
+               for session: Session,
+               completion: @escaping (Result<URLRequest, Error>) -> Void) {
+        if let urlString = urlRequest.url?.absoluteString,
+           urlString.hasPrefix(APIConstant.host) {
+            var urlRequest = urlRequest
+            
+            urlRequest.setValue(AuthorizationToken.token,
+                                forHTTPHeaderField: "Authorization")
+            
+            completion(.success(urlRequest))
+            return
+        }
+        
+        completion(.success(urlRequest))
+    }
+    
+    // API retry function
+    func retry(_ request: Request,
+               for session: Session,
+               dueTo error: Error,
+               completion: @escaping (RetryResult) -> Void) {
+        guard let response = request.task?.response as? HTTPURLResponse,
+              let responseCode = APIErrorResponseCode(rawValue: response.statusCode) else {
+                  completion(.doNotRetryWithError(error))
+                  return
+        }
+        
+        // exceed retry count limit
+        guard request.retryCount < retryCount else {
+            completion(.doNotRetryWithError(error))
+            return
+        }
+        
+        guard !isRefreshing else { return }
+        
+        isRefreshing = true
+        
+        switch responseCode {
+        case .authorizationFailed, .headerLacking:
+            isRefreshing = false
+            completion(.retry)
+        }
+    }
+}
+
---
+++ cleanArchitecture+coordinator/cleanArchitecture+coordinator/Entity/User.swift
@@ -0,0 +1,25 @@
+import ObjectMapper
+
+class User {
+    var description: String?
+    var followCount = 0
+    var followerCount = 0
+    var name: String?
+    var imageUrl: String?
+    
+    init() {
+    }
+    
+    required init(map: Map) {
+    }
+}
+
+extension User: Mappable {
+    func mapping(map: Map) {
+        description <- map["description"]
+        followCount <- map["followees_count"]
+        followerCount <- map["followers_count"]
+        name <- map["name"]
+        imageUrl <- map["profile_image_url"]
+    }
+}
